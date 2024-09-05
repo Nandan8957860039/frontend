156bc98bd031bb148438df2926ccb4bad133a740
nandan8957860039 <sanatanbajpai@gmail.com>
2024-09-05 11:10:30 +0530

    react setup

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl text-center mt-5' >
+      <h1>My Home Page</h1>
+    </div>
+  )
+}
+
+export default Home;
